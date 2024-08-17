70e442338466c9fabfbb2edb62ac1ead49f942d4
Robert Dobyns <robertddobyns@gmail.com>
2024-08-17 00:13:57 -0500

    start header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import MainHeader from './components/main-header/MainHeader'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <MainHeader />
       The Tin Can Co. coming soon!
     </>
   )
---
+++ src/components/main-header/MainHeader.jsx
@@ -0,0 +1,32 @@
+import { styled } from '@mui/material'
+import logo from '../../assets/icon.png'
+
+export default function MainHeader() {
+  return (
+    <Header>
+        <Logo src={logo} />
+    </Header>
+  )
+}
+
+
+const Header = styled('header')(props =>({
+    width: '100%',
+    height: '100px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+}))
+const Logo = styled('img')(props => ({
+    [props.theme.breakpoints.up('xs')]: {
+        height: '100px',
+    },
+    [props.theme.breakpoints.up('sm')]: {
+        height: '200px',
+    },
+    [props.theme.breakpoints.up('md')]: {
+        marginTop: '20px',
+        height: '300px',
+    },
+}))
