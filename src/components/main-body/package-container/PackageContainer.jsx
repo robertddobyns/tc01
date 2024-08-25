5f6ecb6b68cb7758f44d5cd9d0be120738bad5b8
Robert Dobyns <robertddobyns@gmail.com>
2024-08-25 13:28:11 -0500

    Added mutiple sliders

--- src/components/main-body/package-container/PackageContainer.jsx
+++ src/components/main-body/package-container/PackageContainer.jsx
@@ -1,6 +1,9 @@
 import { styled } from "@mui/material";
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { ColorContext } from "../../../App";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 import Package from "../packages/Package";
 
@@ -9,29 +12,142 @@ import Image2 from "../../../assets/photos/demo2.jpg";
 import Image3 from "../../../assets/photos/demo3.jpg";
 import Image4 from "../../../assets/photos/demo4.jpg";
 import Image5 from "../../../assets/photos/demo5.jpg";
+import Image6 from "../../../assets/photos/demo6.jpg";
+import Image7 from "../../../assets/photos/demo7.jpg";
+import Image8 from "../../../assets/photos/demo8.jpg";
+import Image9 from "../../../assets/photos/demo9.jpg";
+import Image10 from "../../../assets/photos/demo10.jpg";
+import Image11 from "../../../assets/photos/demo11.jpg";
+import Image12 from "../../../assets/photos/demo12.jpg";
+import Image13 from "../../../assets/photos/demo13.jpg";
+import Image14 from "../../../assets/photos/demo14.jpg";
+import Image15 from "../../../assets/photos/demo15.jpg";
 
 export default function PackageContainer() {
-
   const [colorTheme, setColorTheme] = useContext(ColorContext);
+
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
 
   return (
     <Container>
       <Column1>
-        <Image className={colorTheme === 'white' ? 'image-white': undefined} src={Image1} alt="image 1" />
-        <Image className={colorTheme === 'white' ? 'image-white': undefined} src={Image2} alt="image 2" />
-        <Image className={colorTheme === 'white' ? 'image-white': undefined} src={Image3} alt="image 3" />
-        <Image className={colorTheme === 'white' ? 'image-white': undefined} src={Image4} alt="image 4" />
-        <Image className={colorTheme === 'white' ? 'image-white': undefined} src={Image5} alt="image 5" />
+        <Slider {...sliderSettings}>
+          <img src={Image9} alt="Bar staff with umbrellas" />
+          <img src={Image10} alt="Smiling Bar staff with cooler" />
+          <img src={Image11} alt="Bar set up" />
+          <img src={Image12} alt="Signature drinks" />
+          <img src={Image13} alt="Married couple on bar" />
+          <img src={Image14} alt="Bar staff assisting patron" />
+          <img src={Image15} alt="Smiling bar staff holding drinks" />
+          <img src={Image1} alt="Bar staff" />
+          <img src={Image2} alt="Bar staff" />
+          <img src={Image3} alt="Welcoming wedding sign" />
+          <img src={Image4} alt="Bar staff showing peace sign" />
+          <img src={Image5} alt="Bar set up" />
+          <img src={Image6} alt="Bar staff with hands on hip" />
+          <img src={Image7} alt="Smiling Bar staff" />
+          <img src={Image8} alt="Bar staff standing behind bar" />
+        </Slider>
+        <Slider {...sliderSettings}>
+          <img src={Image1} alt="Bar staff" />
+          <img src={Image2} alt="Bar staff" />
+          <img src={Image3} alt="Welcoming wedding sign" />
+          <img src={Image4} alt="Bar staff showing peace sign" />
+          <img src={Image5} alt="Bar set up" />
+          <img src={Image6} alt="Bar staff with hands on hip" />
+          <img src={Image7} alt="Smiling Bar staff" />
+          <img src={Image8} alt="Bar staff standing behind bar" />
+          <img src={Image9} alt="Bar staff with umbrellas" />
+          <img src={Image10} alt="Smiling Bar staff with cooler" />
+          <img src={Image11} alt="Bar set up" />
+          <img src={Image12} alt="Signature drinks" />
+          <img src={Image13} alt="Married couple on bar" />
+          <img src={Image14} alt="Bar staff assisting patron" />
+          <img src={Image15} alt="Smiling bar staff holding drinks" />
+        </Slider>
+        <Slider {...sliderSettings}>
+          <img src={Image6} alt="Bar staff with hands on hip" />
+          <img src={Image7} alt="Smiling Bar staff" />
+          <img src={Image8} alt="Bar staff standing behind bar" />
+          <img src={Image9} alt="Bar staff with umbrellas" />
+          <img src={Image10} alt="Smiling Bar staff with cooler" />
+          <img src={Image3} alt="Welcoming wedding sign" />
+          <img src={Image4} alt="Bar staff showing peace sign" />
+          <img src={Image5} alt="Bar set up" />
+          <img src={Image11} alt="Bar set up" />
+          <img src={Image12} alt="Signature drinks" />
+          <img src={Image13} alt="Married couple on bar" />
+          <img src={Image14} alt="Bar staff assisting patron" />
+          <img src={Image15} alt="Smiling bar staff holding drinks" />
+          <img src={Image1} alt="Bar staff" />
+          <img src={Image2} alt="Bar staff" />
+        </Slider>
       </Column1>
       <PackageColumn>
         <Package />
       </PackageColumn>
       <Column3>
-        <Image className={colorTheme === 'white' ? 'image-white': undefined} src={Image5} alt="image 5" />
-        <Image className={colorTheme === 'white' ? 'image-white': undefined} src={Image4} alt="image 4" />
-        <Image className={colorTheme === 'white' ? 'image-white': undefined} src={Image2} alt="image 2" />
-        <Image className={colorTheme === 'white' ? 'image-white': undefined} src={Image3} alt="image 3" />
-        <Image className={colorTheme === 'white' ? 'image-white': undefined} src={Image1} alt="image 1" />
+        <Slider {...sliderSettings}>
+          <img src={Image6} alt="Bar staff with hands on hip" />
+          <img src={Image7} alt="Smiling Bar staff" />
+          <img src={Image8} alt="Bar staff standing behind bar" />
+          <img src={Image9} alt="Bar staff with umbrellas" />
+          <img src={Image10} alt="Smiling Bar staff with cooler" />
+          <img src={Image3} alt="Welcoming wedding sign" />
+          <img src={Image4} alt="Bar staff showing peace sign" />
+          <img src={Image5} alt="Bar set up" />
+          <img src={Image11} alt="Bar set up" />
+          <img src={Image12} alt="Signature drinks" />
+          <img src={Image13} alt="Married couple on bar" />
+          <img src={Image14} alt="Bar staff assisting patron" />
+          <img src={Image15} alt="Smiling bar staff holding drinks" />
+          <img src={Image1} alt="Bar staff" />
+          <img src={Image2} alt="Bar staff" />
+        </Slider>
+        <Slider {...sliderSettings}>
+          <img src={Image9} alt="Bar staff with umbrellas" />
+          <img src={Image10} alt="Smiling Bar staff with cooler" />
+          <img src={Image11} alt="Bar set up" />
+          <img src={Image12} alt="Signature drinks" />
+          <img src={Image13} alt="Married couple on bar" />
+          <img src={Image14} alt="Bar staff assisting patron" />
+          <img src={Image15} alt="Smiling bar staff holding drinks" />
+          <img src={Image1} alt="Bar staff" />
+          <img src={Image2} alt="Bar staff" />
+          <img src={Image3} alt="Welcoming wedding sign" />
+          <img src={Image4} alt="Bar staff showing peace sign" />
+          <img src={Image5} alt="Bar set up" />
+          <img src={Image6} alt="Bar staff with hands on hip" />
+          <img src={Image7} alt="Smiling Bar staff" />
+          <img src={Image8} alt="Bar staff standing behind bar" />
+        </Slider>
+        <Slider {...sliderSettings}>
+          <img src={Image1} alt="Bar staff" />
+          <img src={Image2} alt="Bar staff" />
+          <img src={Image3} alt="Welcoming wedding sign" />
+          <img src={Image4} alt="Bar staff showing peace sign" />
+          <img src={Image5} alt="Bar set up" />
+          <img src={Image6} alt="Bar staff with hands on hip" />
+          <img src={Image7} alt="Smiling Bar staff" />
+          <img src={Image8} alt="Bar staff standing behind bar" />
+          <img src={Image9} alt="Bar staff with umbrellas" />
+          <img src={Image10} alt="Smiling Bar staff with cooler" />
+          <img src={Image11} alt="Bar set up" />
+          <img src={Image12} alt="Signature drinks" />
+          <img src={Image13} alt="Married couple on bar" />
+          <img src={Image14} alt="Bar staff assisting patron" />
+          <img src={Image15} alt="Smiling bar staff holding drinks" />
+        </Slider>
       </Column3>
     </Container>
   );
@@ -40,7 +156,7 @@ export default function PackageContainer() {
 const Container = styled("main")((props) => ({
   display: "flex",
   justifyContent: "space-around",
-  maWidth: '100%'
+  maWidth: "100%",
 }));
 
 const Column1 = styled("div")((props) => ({
@@ -77,5 +193,5 @@ const Column3 = styled("div")((props) => ({
 }));
 
 const Image = styled("img")((props) => ({
-    width: "100%",
-  }));
+  width: "100%",
+}));
--- src/components/mobile-slider/MobileSlider.jsx
+++ src/components/mobile-slider/MobileSlider.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import { styled } from "@mui/material";
 
 import Image1 from "../../assets/photos/demo1.jpg";
 import Image2 from "../../assets/photos/demo2.jpg";
 import Image3 from "../../assets/photos/demo3.jpg";
 import Image4 from "../../assets/photos/demo4.jpg";
 import Image5 from "../../assets/photos/demo5.jpg";
+import Image6 from "../../assets/photos/demo6.jpg";
+import Image7 from "../../assets/photos/demo7.jpg";
+import Image8 from "../../assets/photos/demo8.jpg";
+import Image9 from "../../assets/photos/demo9.jpg";
+import Image10 from "../../assets/photos/demo10.jpg";
+import Image11 from "../../assets/photos/demo11.jpg";
+import Image12 from "../../assets/photos/demo12.jpg";
+import Image13 from "../../assets/photos/demo13.jpg";
+import Image14 from "../../assets/photos/demo14.jpg";
+import Image15 from "../../assets/photos/demo15.jpg";
 
 export default function MobileSlider() {
   const sliderSettings = {
@@ -25,11 +34,21 @@ export default function MobileSlider() {
 
   return (
     <Slider {...sliderSettings}>
-      <img src={Image1} alt="image 1" />
-      <img src={Image2} alt="image 2" />
-      <img src={Image3} alt="image 3" />
-      <img src={Image4} alt="image 4" />
-      <img src={Image5} alt="image 5" />
+      <img src={Image1} alt="Bar staff" />
+      <img src={Image2} alt="Bar staff" />
+      <img src={Image3} alt="Welcoming wedding sign" />
+      <img src={Image4} alt="Bar staff showing peace sign" />
+      <img src={Image5} alt="Bar set up" />
+      <img src={Image6} alt="Bar staff with hands on hip" />
+      <img src={Image7} alt="Smiling Bar staff" />
+      <img src={Image8} alt="Bar staff standing behind bar" />
+      <img src={Image9} alt="Bar staff with umbrellas" />
+      <img src={Image10} alt="Smiling Bar staff with cooler" />
+      <img src={Image11} alt="Bar set up" />
+      <img src={Image12} alt="Signature drinks" />
+      <img src={Image13} alt="Married couple on bar" />
+      <img src={Image14} alt="Bar staff assisting patron" />
+      <img src={Image15} alt="Smiling bar staff holding drinks" />
     </Slider>
   );
 }
